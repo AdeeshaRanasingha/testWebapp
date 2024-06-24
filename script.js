ed3455cb674230574ef1a3172464c0235db283f1
Adeesha Harshana <93689148+adeeshadash@users.noreply.github.com>
2024-06-24 10:31:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+//signup form for first users
+if(localStorage.getItem("name") == null && localStorage.getItem("password") == null){
+
+    
+
+    document.getElementById("reset").style.display="none";
+
+    document.getElementById('heading').innerHTML="sign up"
+
+    document.getElementById('loginBtn').innerHTML = "sign up"
+
+    function login(){
+        let name = document.getElementById("name").value;
+        let password = document.getElementById("password").value;
+        let email = document.getElementById("email").value;
+        let contact = document.getElementById("contact").value;
+
+            //getting details
+        localStorage.setItem("name" , name);
+        localStorage.setItem("password" , password);
+        localStorage.setItem("email" , email);
+        localStorage.setItem("contact" , contact);
+
+        alert("signed up successfully");
+        window.location.href = "index.html";
+    }
+}
+
+//login form
+else{
+
+    
+    document.getElementById("signup").style.display="none"
+
+    //reset the password
+    function reset(){
+
+        document.getElementById("heading").innerHTML="reset form";
+        document.getElementById("reset").style.display="none";
+        document.getElementById("loginBtn").innerHTML="reset";
+        document.getElementById("loginBtn").id="resetBtn";
+    
+        const resetBtn = document.querySelector("#resetBtn");
+    
+        resetBtn.addEventListener("click", login);
+    
+        function login(){
+    
+            let name = document.getElementById("name").value;
+            let password = document.getElementById("password").value;
+            
+            localStorage.setItem("name" , name);
+            localStorage.setItem("password" , password);
+    
+            alert("credentials were successfully changed");
+            window.location.reload();
+    
+        }
+    
+    
+        
+    }
+        //login form
+    function login(){
+        let name = document.getElementById("name").value;
+        let password = document.getElementById("password").value;
+        
+        //if password maches move to the site
+        if(localStorage.getItem("password") == password && localStorage.getItem("name") == name){
+            alert("succesfully loged in");
+
+            document.getElementById("main").style.display="none";
+            window.location.href = "index.html";
+        }
+        else{
+            alert("some thing went wrong");
+        }
+
+    }
+}
+
+
